f5ca02722eb266d2f6c2d8358264ede6fc864112
Nardgelen Jean Francois <nardgelenjf@gmail.com>
2024-04-16 14:31:05 -0400

    added seed data

---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+--populate the database with some data
+
+USE company_db
+
+INSERT INTO department (name) VALUES
+('Engineering'),
+('Sales'),
+('Finance'),
+('Legal');
+
+INSERT INTO role (title, salary, department_id) VALUES
+('Software Engineer', 100000, 1),
+('Sales Lead', 80000, 2),
+('Accountant', 70000, 3),
+('Lawyer', 90000, 4);
+
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES
+('John', 'Doe', 1, NULL),
+('Mike', 'Chan', 2, 1),
+('Ashley', 'Rodriguez', 3, 1),
+('Kevin', 'Tupik', 4, 1),
+('Kunal', 'Singh', 1, 2),
+('Malia', 'Brown', 2, 2),
+('Sarah', 'Lourd', 3, 3),
+('Tom', 'Allen', 4, 3);
+
+
---
+++ server.js
@@ -0,0 +1,170 @@
+const inquirer = require('inquirer');
+const database = require('./config/connection');
+const figlet = require('figlet');
+
+const table = require('console.table');
+
+
+const PORT = process.env.PORT || 3001;
+
+figlet.text('Employee Tracker', {
+    font: 'Standard',
+    horizontalLayout: 'default',
+    verticalLayout: 'default',
+    width: 80,
+    whitespaceBreak: true
+}, function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+    console.log('Welcome to the Employee Tracker');
+    console.log('--------------------------------');
+    console.log('Please select an option below');
+})
+
+const mainMenu = () => {
+    inquirer.prompt([{
+        type: 'list',
+        name: 'mainMenu',
+        message: 'What would you like to do?',
+        choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit']
+    }]).then((response) => {
+        switch (response.mainMenu) {
+            case 'View all departments':
+                viewDepartments();
+                break;
+            case 'View all roles':
+                viewRoles();
+                break;
+            case 'View all employees':
+                viewEmployees();
+                break;
+            case 'Add a department':
+                addDepartment();
+                break;
+            case 'Add a role':
+                addRole();
+                break;
+            case 'Add an employee':
+                addEmployee();
+                break;
+            case 'Update an employee role':
+                updateEmployeeRole();
+                break;
+            case 'Exit':
+                console.log('Goodbye!');
+                process.exit();
+        }
+    })
+}
+
+const viewDepartments = () => {
+    database.query('SELECT * FROM department', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainMenu();
+    })
+}
+
+const viewRoles = () => {
+    database.query('SELECT * FROM role', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainMenu();
+    })
+}
+
+const viewEmployees = () => {
+    database.query('SELECT * FROM employee', (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        mainMenu();
+    })
+}
+
+const addDepartment = () => {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'departmentName',
+        message: 'Enter the name of the department you would like to add'
+    }]).then((response) => {
+        database.query('INSERT INTO department SET ?', { name: response.departmentName }, (err, res) => {
+            if (err) throw err;
+            console.log('Department added successfully');
+            mainMenu();
+        })
+    })
+}
+
+const addRole = () => {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'roleTitle',
+        message: 'Enter the title of the role you would like to add'
+    }, {
+        type: 'input',
+        name: 'roleSalary',
+        message: 'Enter the salary of the role you would like to add'
+    }, {
+        type: 'input',
+        name: 'roleDepartmentId',
+        message: 'Enter the department ID of the role you would like to add'
+    }]).then((response) => {
+        database.query('INSERT INTO role SET ?', { title: response.roleTitle, salary: response.roleSalary, department_id: response.roleDepartmentId }, (err, res) => {
+            if (err) throw err;
+            console.log('Role added successfully');
+            mainMenu();
+        })
+    })
+}
+
+const addEmployee = () => {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'employeeFirstName',
+        message: 'Enter the first name of the employee you would like to add'
+    }, {
+        type: 'input',
+        name: 'employeeLastName',
+        message: 'Enter the last name of the employee you would like to add'
+    }, {
+        type: 'input',
+        name: 'employeeRoleId',
+        message: 'Enter the role ID of the employee you would like to add'
+    }, {
+        type: 'input',
+        name: 'employeeManagerId',
+        message: 'Enter the manager ID of the employee you would like to add'
+    }]).then((response) => {
+        database.query('INSERT INTO employee SET ?', { first_name: response.employeeFirstName, last_name: response.employeeLastName, role_id: response.employeeRoleId, manager_id: response.employeeManagerId }, (err, res) => {
+            if (err) throw err;
+            console.log('Employee added successfully');
+            mainMenu();
+        })
+    })
+}
+
+const updateEmployeeRole = () => {
+    inquirer.prompt([{
+        type: 'input',
+        name: 'employeeId',
+        message: 'Enter the ID of the employee you would like to update'
+    }, {
+        type: 'input',
+        name: 'roleId',
+        message: 'Enter the ID of the role you would like to assign to the employee'
+    }]).then((response) => {
+        database.query('UPDATE employee SET role_id = ? WHERE id = ?', [response.roleId, response.employeeId], (err, res) => {
+            if (err) throw err;
+            console.log('Employee role updated successfully');
+            mainMenu();
+        })
+    })
+}
+
+mainMenu();
+
+// app.listen(PORT, () => {
